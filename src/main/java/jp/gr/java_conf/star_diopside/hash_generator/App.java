12b9a9843016957ad350f6bc61c66744f31bae45
se-yoshi <se.yoshi@gmail.com>
2016-08-27 09:51:16 +0900

    Spring Bootを使用するように変更

--- src/main/java/jp/gr/java_conf/star_diopside/hash_generator/App.java
+++ src/main/java/jp/gr/java_conf/star_diopside/hash_generator/App.java
@@ -1,11 +1,12 @@
-package jp.gr.java_conf.star_diopside.hash_generator.application;
+package jp.gr.java_conf.star_diopside.hash_generator;
 
-import java.io.Closeable;
 import java.nio.charset.StandardCharsets;
 import java.util.ResourceBundle;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -13,18 +14,20 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import jp.gr.java_conf.star_diopside.spark.commons.support.util.CharsetResourceBundleControl;
 
-public class MainApp extends Application {
+@Configuration
+@ImportResource("classpath:applicationContext.xml")
+public class App extends Application {
 
-    private ApplicationContext applicationContext;
+    private ConfigurableApplicationContext applicationContext;
 
     @Override
     public void init() throws Exception {
-        applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        applicationContext = SpringApplication.run(App.class, getParameters().getRaw().toArray(new String[0]));
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/file-hash.fxml"),
+        FXMLLoader loader = new FXMLLoader(applicationContext.getResource("classpath:/fxml/file-hash.fxml").getURL(),
                 ResourceBundle.getBundle("labels", new CharsetResourceBundleControl(StandardCharsets.UTF_8)));
         loader.setControllerFactory(applicationContext::getBean);
 
@@ -35,9 +38,7 @@ public class MainApp extends Application {
 
     @Override
     public void stop() throws Exception {
-        if (applicationContext instanceof Closeable) {
-            ((Closeable) applicationContext).close();
-        }
+        applicationContext.close();
     }
 
     public static void main(String[] args) {
